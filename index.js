1a771549034ce7ab02aaff0ba192e5db8d8145a2
Glitch (mwc-demos) <none>
2020-08-03 20:52:16 +0000

    🌆🎄 Checkpoint ./unbundled.html:20408754/486 ./index.js:20408754/33

--- index.js
+++ index.js
@@ -24,5 +24,6 @@ import '@material/mwc-textarea';
 import '@material/mwc-textfield';
 
 addEventListener('load', function() {
+  console.log("GG")
   document.body.classList.remove('unresolved');
 });
